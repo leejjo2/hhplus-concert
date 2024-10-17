c288829c2902ac09afc74300a9663210fceba11e
jameslee <jungjo4247@naver.com>
2024-10-18 08:13:28 +0900

    feat: ConcertController 구현

--- src/main/java/com/hhplusconcert/concert/controller/ConcertController.java
+++ src/main/java/com/hhplusconcert/concert/controller/ConcertController.java
@@ -2,92 +2,87 @@ package com.hhplusconcert.concert.controller;
 
 import com.hhplusconcert.concert.aop.ConcertTokenRequired;
 import com.hhplusconcert.concert.usecase.*;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.time.LocalDate;
-import java.time.LocalDateTime;
 import java.util.List;
 
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/v1/concerts")
 public class ConcertController implements ConcertControllerInterface {
 
+    private final CreateQueueService createQueueService;
+    private final FindQueueService findQueueService;
+    private final FindAvailableReservationDateService findAvailableReservationDateService;
+    private final FindReservationSeatService findReservationSeatService;
+    private final ReserveSeatService reserveSeatService;
+    private final PurchaseSeatService purchaseSeatService;
+
     @PostMapping("/{concertScheduleId}/queue-token")
     @Override
-    public ResponseEntity<CreateQueueUseCase.Output> createQueue(
+    public ResponseEntity<CreateQueueService.Output> createQueue(
             @PathVariable("concertScheduleId") Long concertScheduleId,
-            @RequestBody CreateQueueUseCase.Input input
+            @RequestBody CreateQueueService.Input input
     ) {
-        String token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl9pZCI6InRva2VuX2lkXzEiLCJ1c2VySWQiOiJleGFtcGxlVXNlciIsImV4cCI6MTcyODYxMjY5Niwid2F0aW5nX251bWJlciI6MTIzNH0.8PooXzqNvBdkgXt6GnMaBGOdSyMvl8ziPdl5M4QSvR4";
-        return ResponseEntity.status(HttpStatus.CREATED).body(new CreateQueueUseCase.Output(token));
+        CreateQueueService.Output output = createQueueService.execute(concertScheduleId, input);
+        return ResponseEntity.status(HttpStatus.CREATED).body(output);
     }
 
     @Override
     @ConcertTokenRequired
     @GetMapping("/{concertScheduleId}/queue")
-    public ResponseEntity<FindQueueUseCase.Output> findQueue(
+    public ResponseEntity<FindQueueService.Output> findQueue(
             @PathVariable("concertScheduleId") Long concertScheduleId
     ) {
-        int waitingNumber = 10; // 실제 대기 번호 조회 로직 필요
-        return ResponseEntity.ok(new FindQueueUseCase.Output(waitingNumber));
+        FindQueueService.Output output = findQueueService.execute(concertScheduleId);
+        return ResponseEntity.ok(output);
     }
 
     @Override
     @ConcertTokenRequired
-    @GetMapping("/{concertScheduleId}/reservation/date")
-    public ResponseEntity<FindReservationDateUseCase.Output> findReservationDate(
-            @PathVariable("concertScheduleId") Long concertScheduleId,
-            @RequestParam(value = "status") String status
+    @GetMapping("/{concertScheduleId}/reservation/available-date")
+    public ResponseEntity<FindAvailableReservationDateService.Output> findReservationDate(
+            @PathVariable("concertScheduleId") Long concertScheduleId
     ) {
-        List<LocalDate> availableDates = List.of(
-                LocalDate.parse("2024-10-09"),
-                LocalDate.parse("2024-10-10")
-        );
-        return ResponseEntity.ok(new FindReservationDateUseCase.Output(availableDates));
+        FindAvailableReservationDateService.Output output = findAvailableReservationDateService.execute(concertScheduleId);
+        return ResponseEntity.ok(output);
     }
 
     @Override
     @ConcertTokenRequired
     @GetMapping("/{concertScheduleId}/reservation/seat")
-    public ResponseEntity<List<FindReservationSeatUseCase.Output>> findReservationSeat(
-            @PathVariable("concertScheduleId") Long concertScheduleId,
-            @RequestParam(value = "concertOpenDate") LocalDate concertOpenDate
+    public ResponseEntity<List<FindReservationSeatService.Output>> findReservationSeat(
+            @PathVariable("concertScheduleId") Long concertScheduleId
     ) {
-        List<FindReservationSeatUseCase.Output> seats = List.of(
-                new FindReservationSeatUseCase.Output(1L, 1, 10000, "AVAILABLE"),
-                new FindReservationSeatUseCase.Output(2L, 2, 10000, "TEMP_RESERVED"),
-                new FindReservationSeatUseCase.Output(3L, 3, 10000, "RESERVED")
-        );
-        return ResponseEntity.ok(seats);
+        List<FindReservationSeatService.Output> output = findReservationSeatService.execute(concertScheduleId);
+        return ResponseEntity.ok(output);
     }
 
     @Override
     @ConcertTokenRequired
     @PostMapping("/{concertScheduleId}/reservation/seat/{seatId}")
-    public ResponseEntity<Void> reserveSeat(
+    public ResponseEntity<ReserveSeatService.Output> reserveSeat(
             @PathVariable("concertScheduleId") Long concertScheduleId,
-            @PathVariable("seatId") Long seatId
+            @PathVariable("seatId") Long seatId,
+            @RequestBody ReserveSeatService.Input input
     ) {
-        return ResponseEntity.status(HttpStatus.CREATED).build();
+
+        ReserveSeatService.Output output = reserveSeatService.execute(concertScheduleId, seatId, input);
+        return ResponseEntity.ok(output).status(HttpStatus.CREATED).build();
     }
 
     @Override
     @ConcertTokenRequired
-    @PostMapping("/{concertScheduleId}/purchase/seat/{seatId}")
-    public ResponseEntity<PurchaseSeatUseCase.Output> purchaseSeat(
-            @PathVariable("concertScheduleId") Long concertScheduleId,
-            @PathVariable("seatId") Long seatId,
-            @RequestBody PurchaseSeatUseCase.Input input
+    @PostMapping("/{reservationId}/purchase")
+    public ResponseEntity<PurchaseSeatService.Output> purchaseSeat(
+            @PathVariable("reservationId") Long reservationId,
+            @RequestBody PurchaseSeatService.Input input
     ) {
-        return ResponseEntity.status(HttpStatus.CREATED).body(new PurchaseSeatUseCase.Output(
-                1L,
-                "concert name",
-                LocalDate.of(2024, 10, 19), // 콘서트 날짜
-                333, // 구매한 좌석 번호
-                150000, // 좌석 가격
-                LocalDateTime.of(2024, 10, 1, 0, 0, 0) // 구매 시간
-        ));
+
+        PurchaseSeatService.Output output = purchaseSeatService.execute(reservationId, input);
+        return ResponseEntity.status(HttpStatus.CREATED).body(output);
     }
 }
--- src/main/java/com/hhplusconcert/concert/controller/ConcertControllerInterface.java
+++ src/main/java/com/hhplusconcert/concert/controller/ConcertControllerInterface.java
@@ -5,62 +5,59 @@ import com.hhplusconcert.concert.usecase.*;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
-import java.time.LocalDate;
 import java.util.List;
 
 public interface ConcertControllerInterface {
     @Operation(summary = "대기열 토큰 생성", description = "특정 콘서트 일정에 대한 대기열 토큰을 생성합니다.")
     @PostMapping("/{concertScheduleId}/queue-token")
-    ResponseEntity<CreateQueueUseCase.Output> createQueue(
+    ResponseEntity<CreateQueueService.Output> createQueue(
             @Parameter(description = "콘서트 일정 ID", required = true)
             @PathVariable("concertScheduleId") Long concertScheduleId,
-            @RequestBody CreateQueueUseCase.Input input
+            @RequestBody CreateQueueService.Input input
     );
 
     @Operation(summary = "대기열 조회", description = "특정 콘서트 일정에 대한 대기열 정보를 조회합니다.")
     @GetMapping("/{concertScheduleId}/queue")
     @ConcertTokenRequired
-    ResponseEntity<FindQueueUseCase.Output> findQueue(
+    ResponseEntity<FindQueueService.Output> findQueue(
             @Parameter(description = "콘서트 일정 ID", required = true)
             @PathVariable("concertScheduleId") Long concertScheduleId
     );
 
     @Operation(summary = "예약 가능한 날짜 조회", description = "예약 가능한 날짜 목록을 조회합니다.")
-    @GetMapping("/{concertScheduleId}/reservation/date")
-    ResponseEntity<FindReservationDateUseCase.Output> findReservationDate(
+    @GetMapping("/{concertScheduleId}/reservation/available-date")
+    ResponseEntity<FindAvailableReservationDateService.Output> findReservationDate(
             @Parameter(description = "콘서트 일정 ID", required = true)
-            @PathVariable("concertScheduleId") Long concertScheduleId,
-            @Parameter(description = "예약 상태 필터", required = true)
-            @RequestParam(value = "status") String status
+            @PathVariable("concertScheduleId") Long concertScheduleId
     );
 
     @Operation(summary = "좌석 상태 조회", description = "특정 콘서트 일정에 대한 좌석 상태를 조회합니다.")
     @GetMapping("/{concertScheduleId}/reservation/seat")
-    ResponseEntity<List<FindReservationSeatUseCase.Output>> findReservationSeat(
+    ResponseEntity<List<FindReservationSeatService.Output>> findReservationSeat(
             @Parameter(description = "콘서트 일정 ID", required = true)
-            @PathVariable("concertScheduleId") Long concertScheduleId,
-            @Parameter(description = "콘서트 시작 날짜", required = true)
-            @RequestParam(value = "concertOpenDate") LocalDate concertOpenDate
+            @PathVariable("concertScheduleId") Long concertScheduleId
     );
 
     @Operation(summary = "좌석 예약", description = "특정 콘서트 일정의 좌석을 예약합니다.")
     @PostMapping("/{concertScheduleId}/reservation/seat/{seatId}")
-    ResponseEntity<Void> reserveSeat(
-            @Parameter(description = "콘서트 일정 ID", required = true)
-            @PathVariable("concertScheduleId") Long concertScheduleId,
-            @Parameter(description = "좌석 ID", required = true)
-            @PathVariable("seatId") Long seatId
-    );
-
-    @Operation(summary = "좌석 구매", description = "대기열 토큰을 사용하여 좌석을 구매합니다.")
-    @PostMapping("/{concertScheduleId}/purchase/seat/{seatId}")
-    ResponseEntity<PurchaseSeatUseCase.Output> purchaseSeat(
+    ResponseEntity<ReserveSeatService.Output> reserveSeat(
             @Parameter(description = "콘서트 일정 ID", required = true)
             @PathVariable("concertScheduleId") Long concertScheduleId,
             @Parameter(description = "좌석 ID", required = true)
             @PathVariable("seatId") Long seatId,
-            @RequestBody PurchaseSeatUseCase.Input input
+            @RequestBody ReserveSeatService.Input input
+    );
+
+    @Operation(summary = "좌석 구매", description = "대기열 토큰을 사용하여 좌석을 구매합니다.")
+    @PostMapping("/{reservationId}/purchase")
+    ResponseEntity<PurchaseSeatService.Output> purchaseSeat(
+            @Parameter(description = "예약 ID", required = true)
+            @PathVariable("reservationId") Long reservationId,
+            @RequestBody PurchaseSeatService.Input input
     );
 }
